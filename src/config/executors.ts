adfdfe01c501440725a3f11692067c845c9aa808
mallusrgreat <mallusrgreat@duck.com>
2024-04-17 12:37:49 +0530

    chore: add warning to hydrogen

--- src/config/executors.ts
+++ src/config/executors.ts
@@ -23,6 +23,7 @@ export const executors: Executor[] = [
     icon: "/executors/hydrogen.png",
     discord: "https://discord.gg/hydrogen",
     website: "https://hydrogen.sh",
+    warning: "Android version doesn't work, but the MacOS version works.",
   },
   {
     name: "Wave",
